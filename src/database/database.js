961cc0c243ddc0ff84bc0b3e82d0fb1f1415667c
Mern-Tapas <cyb6261452510@gmail.com>
2022-10-06 21:48:05 +0530

    ad prot

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,3 @@
 const mongoose = require("mongoose");
 
-const db = "mongodb+srv://tapas_0:yamahavb5850@cluster0.adudr.mongodb.net/?retryWrites=true&w=majority"
-
-mongoose.connect("mongodb://localhost:27017/rdata").then(()=>{console.log("connected with database")}).catch(()=>{"not connected"})
-// mongoose.connect(db).then(()=>{console.log("connected")}).catch(()=>{"not connected"})
+mongoose.connect(process.env.DB).then(()=>{console.log("connected with database")}).catch(()=>{"not connected"})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 require("./database/database");
-const port = 4000;
+const port = process.env.PORT;
 const app = express();
 const hbs = require("hbs");
 const path = require("path");
